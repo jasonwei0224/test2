1fa915f66bcd45294b28dd5149c4e3aaaf6c2d14
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-06-26 15:47:01 -0700

    Refactor ProgramInfo

--- src/programInfo.js
+++ src/programInfo.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Button, Row, Col } from 'react-bootstrap';
-// import './programInfo.css'
+import './programInfo.css'
 
 
 const ProgramInfo = (props) => {
     return (
         <div>
             {/*<Row className="borderLine"></Row>*/}
-            <Row className="contents">
+            <Row className="PI-contents">
                 <Col lx={{span:8, offset:2}} lg={{span:10, offset:1}} md={{span:10, offset:1}} sm={{span:10, offset:1}}  xs={{span:8, offset:2}}>
-                    <Row><p className="subtitle">{props.subtitle}</p></Row>
-                    <Row><h1 className="title">{props.title}</h1></Row>
-                    <Row className="last">
-                        <Col className="artist"><p>{props.artistName}</p></Col>
-                        <div className="col-button"><Button className="share" href={props.url} style={{fontFamily:"sofia-pro"}}>SHARE WITH FRIENDS</Button></div>
+                    <Row><p className="PI-subtitle">{props.subtitle}</p></Row>
+                    <Row><h1 className="PI-title">{props.title}</h1></Row>
+                    <Row className="PI-last">
+                        <Col className="PI-artist"><p>{props.artistName}</p></Col>
+                        <div className="PI-colButton"><Button className="PI-shareButton" href={props.url}>SHARE WITH FRIENDS</Button></div>
                     </Row>
                 </Col>
             </Row>
-            <Row className="borderLineBottom" style={{borderBottom:"60px solid "+props.color}}></Row>
+            <Row className="PI-borderLineBottom" style={{borderBottom:"60px solid "+props.color}}></Row>
 
         </div>
     );
