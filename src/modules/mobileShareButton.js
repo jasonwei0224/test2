f73666ea695ed8dd5d7eb7bd375cfacb06f3e4ca
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-07-07 11:03:30 +0800

    update other side of mask using components

--- src/modules/mobileShareButton.js
+++ src/modules/mobileShareButton.js
@@ -3,15 +3,9 @@ import { Button, Row, Col } from 'react-bootstrap';
 import './ButtonModule.css'
 
 
-const Button2 = (props) => {
+const MobileShareButton = (props) => {
     return (
         <div>
-            <Row>
-                <Col style={{ marginBottom: "50px", textAlign:"center"}} xl={{span:4, offset:4}} lg={{span:8, offset:2}}sm={{span:10, offset:1}}xs={{span:10, offset:1}}>
-                    <Button onClick={props.onclick} bsPrefix="uploadBtnLong"> {props.title}</Button>
-                </Col>
-            </Row>
-
             <Row>
                 <Col style={{ marginBottom: "50px", textAlign:"center"}} xl={{span:4, offset:4}} lg={{span:8, offset:2}}sm={{span:10, offset:1}}xs={{span:10, offset:1}}>
                     <Button variant="primary" type="submit" bsPrefix="mobileShareBtn" ><a href={props.togoUrl}>SHARE WITH FRIENDS</a></Button>
@@ -22,4 +16,4 @@ const Button2 = (props) => {
     );
 }
 
-export default Button2;
+export default MobileShareButton;
