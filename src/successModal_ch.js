66afc44c1213b5a2b22af8c791a87e97d1739aa3
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-07-10 08:14:19 +0800

    make ch form refresh on submit

--- src/successModal_ch.js
+++ src/successModal_ch.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import './modalStyle.css'
 
 
-function SuccessModal_ch(props) {
+class SuccessModal_ch extends Component {
+  refreshPage() {
+    window.location.reload(false);
+  }
+  render() {
     return (
-        <Modal show={props.show}>
-            <Modal.Header className="modalHeader">表格已送出</Modal.Header>
-            <Modal.Body className="modalContents">謝謝您的參與！</Modal.Body>
-            <Modal.Footer>
-                <Button className="btnCancelIn_Modal"  onClick={props.onClick} >關閉</Button>
-            </Modal.Footer>
-        </Modal>
+      <Modal show={this.props.show}>
+          <Modal.Header className="modalHeader">表格已送出</Modal.Header>
+          <Modal.Body className="modalContents">謝謝您的參與！</Modal.Body>
+          <Modal.Footer>
+              <Button className="btnCancelIn_Modal"  onClick={() => { this.props.onClick(); this.refreshPage();}} >關閉</Button>
+          </Modal.Footer>
+      </Modal>
     );
+  }
 }
 
 export default SuccessModal_ch;
