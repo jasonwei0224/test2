ec8e995979edafd47e33ba0d4652e41d3fd2b246
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-07-12 22:57:21 +0800

    complete update phph

--- public/footer_mail.php
+++ public/footer_mail.php
@@ -3,14 +3,14 @@
 if(count($_POST)>0){
   $to = "jasonwei0224@gmail.com";
   $subject = "New Submission Toronto TAIWANfest Website Contact Form";
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
   $message = '<html><body>';
   $message .= "<h4> Contact Us </h4>";
-  $message .= "<strong> Name: </strong>" .trim($_POST['contact_email']) "<br>";
-  $message .= "<strong> Email: </strong>" .trim($_POST['contact_name']) "<br>";
-  $message .= "<strong> Message: </strong>" .trim($_POST['message']) "<br>";
+  $message .= "<strong> Name: </strong>" .trim($_POST['contact_email']) ."<br>";
+  $message .= "<strong> Email: </strong>" .trim($_POST['contact_name']) ."<br>";
+  $message .= "<strong> Message: </strong>" .trim($_POST['message']) ."<br>";
   $message .= "</body></html>";
-
-  $headers = "From: jasonwei0224@gmail.com";
 
   if(mail($to,$subject,$message,$headers)){
     echo "success";
