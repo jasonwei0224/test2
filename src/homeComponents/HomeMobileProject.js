30cceca5b22026b748960267c261b9291062bf07
Gina <ginakim.jh@gmail.com>
2020-07-14 13:14:08 -0700

    mobile version completed -need to resize img and change font style

---
+++ src/homeComponents/HomeMobileProject.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import homepage_placeholder from "../assets/placeholder.jpg";
+/**
+ * Third section in Homepage.
+ * Props: title - project title.
+ * @param {*} props
+ */
+function HomeMobileProject(props) {
+    return (
+        <div id="homeMobileProjectRight">
+            {/* <Row className="homeMobileProject no-gutters">
+                <Col xs="8"className="homeProjectBlankCol"></Col>
+                <Col xs="4"className="homeProjectTitleCol">{props.title}</Col>
+            </Row> */}
+            <div>
+                <img id="homeMobileProjectImg" src={homepage_placeholder}/>
+            </div>
+            <div id="homeMobileProjectBottomSection">
+                <div id="homeMobileProjectType">{props.type}</div>
+                <hr id="homeMobileProjectLine"/>
+                <div id="homeMobileProjectTitle">{props.title}</div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default HomeMobileProject;
--- src/homeComponents/home_v2.js
+++ src/homeComponents/home_v2.js
@@ -8,7 +8,8 @@ import HomeProjectRight from './HomeProjectRight';
 import HomeProjectLeft from './HomeProjectLeft';
 import SponsorImgSlider from './SponsorImgSlider';
 import HomeColoredHeader from './home_colored_header';
-
+import HomeMobileIntro from './homeMobileIntro';
+import HomeMobileProject from './HomeMobileProject';
 
 function Home2() {
     return (
@@ -16,7 +17,8 @@ function Home2() {
         <Row id="homeFirstRow">
             <HomeTopSection/>
         </Row>
-        <HomeSecondSection header="header"/>
+        <HomeMobileIntro/>
+        <HomeSecondSection id="homeSecondSection" header="header"/>
         <div id="homeThirdSection">
             <HomeColoredHeader title="I'M DIFFERENT, JUST LIKE YOU!"/>
             <HomeProjectRight title="UNDER THE SAME SKY PROJECT"/>
@@ -28,6 +30,18 @@ function Home2() {
             <HomeProjectLeft title="PAUL'S APPLES"/>
             <HomeProjectRight title="PEOPLE AND MASK"/>
             <SponsorImgSlider id="sponsorSlider"/>
+        </div>
+        <div id="homeThirdSectionMobile">
+            <HomeColoredHeader title="I'M DIFFERENT, JUST LIKE YOU!"/>
+            <HomeMobileProject type="INTERACTIVE DESIGN" title="UNDER THE SAME SKY PROJECT"/>
+            <HomeMobileProject type="INTERACTIVE DESIGN" title="WE ARE ALL IN THIS TOGETHER"/>
+            <HomeMobileProject type="GRAPHIC ART" title="(UN) BEING CHINESE"/>
+            <HomeMobileProject type="ILLUSTRATION" title="MIRRORS"/>
+            <HomeColoredHeader title="BEHIND THE MASK"/>
+            <HomeMobileProject type="CREATIVE ART" title="THE OTHER SIDE OF MASK"/>
+            <HomeMobileProject type="FINE ART" title="PAUL'S APPLE"/>
+            <HomeMobileProject type="SKETCH ART" title="PEOPLE AND MASK"/>
+            
         </div>
         </div>
     )
