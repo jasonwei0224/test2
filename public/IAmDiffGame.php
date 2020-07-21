4b196080dfe4f3689e0cd68fb45f64f16a852831
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-07-21 15:24:35 +0800

    add php to pass in filename

--- public/IAmDiffGame.php
+++ public/IAmDiffGame.php
@@ -3,8 +3,17 @@
 $dir = "./subjectPhotos/";
 $filenames = scandir($dir);
 $filenamearray = \array_diff($filenames, [".", ".."]);
-count($filenamearray);
+$index = [];
+$fileNameFinal = [];
+$numOfFile = count($filenamearray);
+for ($i = 0; $i < 36; $i++) {
+  array_push($index, rand(2,$numOfFile-1));
+}
 
-echo json_encode($filenamearray);
+for ($i = 0; $i< 36;$i++){
+  $num = $index[$i];
+    array_push($fileNameFinal, $filenamearray[$num]);
+}
+echo json_encode($index).json_encode($fileNameFinal);
 
 ?>
