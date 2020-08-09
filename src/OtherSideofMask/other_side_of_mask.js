5c40597198d0c01b7366f6c296ce201402d5c8cb
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-08-09 12:20:46 +0800

    update mirror animation

--- src/OtherSideofMask/other_side_of_mask.js
+++ src/OtherSideofMask/other_side_of_mask.js
@@ -33,18 +33,18 @@ class OtherSideOfMask extends Component {
       <div>
         <Container fluid>
           <Row>
-            <Col xl={{span:6, offset:0}} lg={{span:11, offset:1}} md= {{span:10, offset:1}} xs={{span:10, offset:1}}className="model3D-col" >
+            <Col xl={{span:6, offset:0}} lg={{span:12, offset:0}} md= {{span:12, offset:0}} xs={{span:12, offset:0}}className="model3D-col" >
               <div>
-                <OBJModel src="./Bee.obj" textPath="" width="500" height="500"
+                <OBJModel src="./Bee.obj" textPath="" width="1800" height="600"
                   position={{x:0,y:-10,z:0}} style={{backgroundColor:"black"}}/>
               </div>
             </Col>
             {/*}<Col className="middle_text_other" xl={{span:0, offset:0}} lg={{span:11, offset:1}} md= {{span:10, offset:1}} xs={{span:10, offset:1}}>
               <MainContents_Middle_Adonis style={{color:"#0C3866"}} font="adonis-web" contents={text}>          </MainContents_Middle_Adonis>
             </Col>*/}
-            <Col xl={{span:6, offset:0}} lg={{span:11, offset:1}} md= {{span:10, offset:1}} xs={{span:10, offset:1}} className="model3D-col" >
+            <Col xl={{span:6, offset:0}} lg={{span:12, offset:0}} md= {{span:12, offset:0}} xs={{span:12, offset:0}} className="model3D-col" >
             <div>
-              <OBJModel src="./Bee.obj" textPath="" width="500" height="500"
+              <OBJModel src="./Bee.obj" textPath="" width="1800" height="600"
                   position={{x:0,y:-10,z:0}} style={{backgroundColor:"black"}}/>
               </div>
             </Col>
--- src/ch/OtherSideOfMask-ch/other_side_of_mask-ch.js
+++ src/ch/OtherSideOfMask-ch/other_side_of_mask-ch.js
@@ -33,20 +33,23 @@ class OtherSideOfMask_ch extends Component {
         return (
           <div>
             <Container fluid>
-            <Row>
-              <Col lg={6} className ="model3D-col-ch">
-              <div>
-                <OBJModel src="./Bee.obj" textPath="" width="500" height="500"
-                   position={{x:0,y:-10,z:0}} style={{backgroundColor:"black"}}/>
-               </div>
-              </Col>
-              <Col lg={6} className ="model3D-col-ch">
-              <div>
-                <OBJModel src="./Bee.obj" textPath="" width="500" height="500"
-                   position={{x:0,y:-10,z:0}} style={{backgroundColor:"black"}}/>
-               </div>
-              </Col>
-            </Row>
+              <Row>
+                <Col xl={{span:6, offset:0}} lg={{span:12, offset:0}} md= {{span:12, offset:0}} xs={{span:12, offset:0}}className="model3D-col" >
+                  <div>
+                    <OBJModel src="./Bee.obj" textPath="" width="1800" height="600"
+                      position={{x:0,y:-10,z:0}} style={{backgroundColor:"black"}}/>
+                  </div>
+                </Col>
+                {/*}<Col className="middle_text_other" xl={{span:0, offset:0}} lg={{span:11, offset:1}} md= {{span:10, offset:1}} xs={{span:10, offset:1}}>
+                  <MainContents_Middle_Adonis style={{color:"#0C3866"}} font="adonis-web" contents={text}>          </MainContents_Middle_Adonis>
+                </Col>*/}
+                <Col xl={{span:6, offset:0}} lg={{span:12, offset:0}} md= {{span:12, offset:0}} xs={{span:12, offset:0}} className="model3D-col" >
+                <div>
+                  <OBJModel src="./Bee.obj" textPath="" width="1800" height="600"
+                      position={{x:0,y:-10,z:0}} style={{backgroundColor:"black"}}/>
+                  </div>
+                </Col>
+              </Row>
             {/*<Row className="other-side-hint-ch"><p>Click and drag or scroll to look around</p></Row>*/}
 
             <ProgramInfoTwoArtistCH
